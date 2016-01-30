ec765e58a2cde96b2fc7f79093a251917a1a0a04
gauravalgo <gauravalgo@gmail.com>
2016-01-30 17:16:56 +0530

    Implemented header of String class for Game_Engine

--- gengine/String.h
+++ gengine/String.h
@@ -1,5 +1,14 @@
 class String
 {
- //this is for test
+    //this is for test
+    private:
+        int strlength;
+        char *buffer;
+    public:
+    String():strlength(0),buffer(NULL);//Default constructor
+    String(char ch);                //pamaterised character constructor
+    String(const char *cstring=""); //paramaterised const character string constructor
+    String(const char &str);        //copy constructor
+    ~String();                      //destructor
 };
 
