efcf6f2d4ce8e84280d60e228fa5869290f8bdc8
gauravalgo <gauravalgo@gmail.com>
2016-02-01 23:27:12 +0530

    I am adding Logger for my game engine without String class feature

--- gengine/main.cpp
+++ gengine/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
+#include "Logger.h"
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+    Logger().GET(__FILE__,__LINE__,__func__,DEBUGWARNING)<<"this is great"<<endl;
     return 0;
 }
