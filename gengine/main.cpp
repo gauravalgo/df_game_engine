6f9b99d698367afeab52d19a7c776734a6aa95c6
gauravalgo <gauravalgo@gmail.com>
2016-02-02 10:33:46 +0530

    creating Logging as Macro

--- gengine/main.cpp
+++ gengine/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "Logger.h"
 using namespace std;
-
+#define LOG(level,char* str)  Logger().GET(__FILE__,__LINE__,__func__,DEBUGWARNING)<<str;
 int main()
 {
     cout << "Hello world!" << endl;
