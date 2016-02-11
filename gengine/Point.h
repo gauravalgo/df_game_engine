7e676f80c9f86bb307509ac556e4719b5d21dfd7
gaurav sharma <gauravalgo@gmail.com>
2016-02-11 18:34:47 +0530

    adding namespace comments to points class

--- gengine/Point.h
+++ gengine/Point.h
@@ -1,39 +1,45 @@
-class Point
+namespace D2
 {
-    private:
-    double x;
-    double y;
-    public:
-    Point(double x=0.0,double y=0.0)
+    class Point
     {
-        this->x=x;
-        this->y=y;
-    }
-    Point(const Point& P)
-    {
-        this->x=P.x;
-        this->y=P.y;
-    }
-    Point& operator=(const Point& P)
-    {
-        this->x=P.x;
-        this->y=P.y;
-    }
-    /*Returns the X coordinate of this Point2D in double precision.*/
+        private:
+        double x;
+        double y;
+        public:
+        Point(double x=0.0,double y=0.0)
+        {
+            this->x=x;
+            this->y=y;
+        }
+        Point(const Point& P)
+        {
+            this->x=P.x;
+            this->y=P.y;
+        }
+        Point& operator=(const Point& P)
+        {
+            this->x=P.x;
+            this->y=P.y;
+        }
+    /*Sets the X coordinate of this Point2D in double precision.*/
     void setX(double x=0.0)
     {
         this->x=x;
     }
-    void setY(double y=0.0)
+     /*Sets the Y coordinate of this Point2D in double precision.*/
+    void	setY(double y=0.0)
     {
         this->y=y;
     }
+   /*Sets the X coordinate of this Point2D in double precision.*/
     double	getX()
     {
         return x;
     }
+    /*Sets the Y coordinate of this Point2D in double precision.*/
     double	getY()
     {
         return y;
     }
+};
 };
