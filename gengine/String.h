9dcf9c4c2b4a3c2c18dab1ad8a953bebe006e4fa
gaurav sharma <gauravalgo@gmail.com>
2016-02-19 11:08:13 +0530

    adding operator overloding

--- gengine/String.h
+++ gengine/String.h
@@ -15,5 +15,26 @@ class String
     String(const char *cstring);   //paramaterised const character string constructor
     String(const String &str);          //copy constructor
     ~String();                        //destructor
+    
+    //concating two Strings using += operator
+    String& operator+=(const char *cstring);
+    String& operator+=(const String& cstring);
+    String operator+(String cstring);
+    
+    bool operator<(String cstring);
+    bool operator<=(String cstring);
+    bool operator>(String cstring);
+    bool operator>=(String cstring);
+    bool operator==(String cstring);
+    bool operator!=(String cstring);
+   
+   //String functions
+   void clear();
+   String Lower();
+   String Upper();
+   int Length();
+   
+   //casting operators 
+   operator const char*();
 };
 
