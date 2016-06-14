7519c46a323cbfb1cce8ddfdd352d08973ad5046
gauravalgo <gauravalgo@gmail.com>
2016-06-15 00:42:22 +0530

    merging with Logging and String.

--- gengine/main.cpp
+++ gengine/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include "Logger.h"
+/*#define LOG(level,char* str)  Logger().GET(__FILE__,__LINE__,__func__,DEBUGWARNING)<<str;*/
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    String s="gaurav";
+    String s1="sharma";
+    String s3=s1+s;
+   Logger::log(1,s3);
     return 0;
 }
